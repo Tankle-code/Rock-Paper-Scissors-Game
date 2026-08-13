253a4abf0c6be4c6b30055a6f48ac0e9315c1afe
Loki <tienle.ftu@gmail.com>
2026-08-13 16:12:31 +0700

    Get the human choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,6 +2,11 @@
 function getComputerChoice() {
     const range = Math.random() * 3;
     if (range <1) {return "Rock"}
-    else if (range <1) {return "Scissors"}
+    else if (range <2) {return "Scissors"}
     else { return "Paper"}
+}
+
+// Get the Human choice 
+function getHumanChoice() {
+    return prompt("Do you want to choose Rock, Scissors, or Paper?")
 }
